54e33d4792a0a9775f886902db52e5f311648edc
Alex <i.aleksey.yakovlev@gmail.com>
2016-06-24 18:45:28 +0200

    Added Google analytics

--- index.php
+++ index.php
@@ -444,5 +444,15 @@ echo $image->render($renderrer);</code></pre>
         $(".bs-docs-sidebar").toggleClass("fixed top", $(window).scrollTop() > 370)
     });
 </script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-79851443-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
